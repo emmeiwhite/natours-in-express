09058732389b551891018c79e7996321df111e71
Imran Rafiq Rather <emmeiwhite@gmail.com>
2024-06-15 00:15:47 +0530

    basic tours api practice

--- app.js
+++ app.js
@@ -1,8 +1,30 @@
 const express = require('express');
+const fs = require('fs');
 const app = express();
 
+/*
 app.get('/', (req, res) => {
-  res.status(200).send('Hello from the server side');
+  //   res.status(200).send('Hello from the server side');
+  res
+    .status(200)
+    .json({ message: 'Hello from the server side', app: 'Natours' });
+});
+
+app.post('/', (req, res) => {
+  res.send('You can post to this end-point');
+});
+*/
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours.json`, 'utf-8')
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).send({
+    status: 'success',
+    data: {
+      tours,
+    },
+  });
 });
 
 const port = '5000';

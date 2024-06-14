3abc7f583fd01f594be153765487aab17059bdec
Imran Rafiq Rather <emmeiwhite@gmail.com>
2024-06-14 18:11:31 +0530

    Basic route set for app.get('/')

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
 
-const port = '8000';
+app.get('/', (req, res) => {
+  res.status(200).send('Hello from the server side');
+});
 
+const port = '5000';
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
